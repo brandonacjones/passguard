59148cfe6e2a724dc4cc66980fa992d0d5b475de
brandonacjones <brandon.ac.jones@gmail.com>
2024-11-21 16:29:50 -0800

    Finished update blueprint, Credential updates are now functional

--- passguard/pages.py
+++ passguard/pages.py
@@ -76,31 +76,34 @@ def decrypt_on_demand(service_id):
 
 @bp.route('/update', methods=['GET', 'POST'])
 def update():
-    error = None;
-    new_username = request.form.get('service_username')
-    new_name = request.form.get('service_name')
-    new_password = request.form.get('service_password')
-    new_url = request.form.get('service_url')
+    if request.method == 'POST':
+        error = None;
+        new_username = request.form.get('service_username')
+        new_name = request.form.get('service_name')
+        new_password = request.form.get('service_password')
+        new_url = request.form.get('service_url')
 
-    encrypted_new_password = encrypt_password(new_password, session['session_user_username'])
+        if not new_username or not new_name or not new_password or not new_url:
+            error = "Missing field in update form. Ensure all fields are filled in."
 
-    if not new_username or not new_name or not new_password or not new_url:
-        error = "Missing field in update form. Ensure all fields are filled in."
+        encrypted_new_password = encrypt_password(new_password, session['session_user_username'])
 
-    db = get_db()
-    if error is None:
-        try:
-            db.execute(
-                'UPDATE service_name, service_username, service_password, service_url IN services'
-                ' VALUES (?, ?, ?, ?) WHERE user_id = ?',
-                (new_name, new_username, encrypted_new_password, new_url, session['session_user_username'])
-            )
-            db.commit()
+        db = get_db()
+        if error is None:
+            try:
+                db.execute(
+                    'UPDATE services SET service_name = ?, service_username = ?, service_password = ?, service_url = ?'
+                    ' WHERE service_id = ? AND user_id = ?',
+                    (new_name, new_username, encrypted_new_password, new_url, session['service_id'], session['session_user_id'])
+                )
+                db.commit()
+                return redirect(url_for('pages.dashboard'))
+            except:
+                print("An Error Occurred during the database update operation.")
+            else:
+                redirect(url_for('pages.dashboard'))
+        redirect(url_for('pages.dashboard'))
 
-        except:
-            print("An Error Occurred during the database update operation.")
-        else:
-            redirect(url_for('pages.dashboard'))
     return render_template('pages/update.html')
 
 @bp.route('/process-service-id', methods=['POST'])
@@ -123,6 +126,7 @@ def process_service_id():
     session['service_username'] = service_details['service_username']
     session['service_password_plain'] = decrypted_service_password
     session['service_url'] = service_details['service_url']
+    session['service_id'] = service_id
 
     return jsonify({"message": "ServiceID processed successfully."})
     
